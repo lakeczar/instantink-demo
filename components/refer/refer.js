b5a33ebd1c034875514177bf44ca6cae2e8186c0
Alek Racz <raczaig@gmail.com>
2021-07-10 14:00:27 -0500

    fixed some off screen hero box on refers area

--- components/refer/refer.js
+++ components/refer/refer.js
@@ -4,9 +4,9 @@ const Refer = () => {
   return (
     <div>
       <div className="my-10"></div>
-        <div className="flex h-full mx-auto max-w-screen-2xl mb-14 my-auto flex-col sm:flex-row">
+        <div className="flex h-auto mx-auto max-w-screen-2xl mb-14 my-auto sm:flex-row">
 
-          <div className="max-w-screen-2xl sm:w-10/12 z-0 flex-shrink-0" style={{
+          <div className="max-w-screen-2xl w-full sm:w-10/12 z-0 flex-shrink-0" style={{
             backgroundImage: `url(./images/hero/referHero.webp)`,
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover',
@@ -14,7 +14,7 @@ const Refer = () => {
             height:'550px',
             }}>
           </div>
-          <div className="relative transform -translate-y-96 sm:-translate-x-60 sm:translate-y-0 z-10 flex-grow-0 flex-shrink-0 my-36 p-6 bg-white max-w-md">
+          <div className="relative transform -translate-y-96 sm:-translate-x-60 sm:translate-y-0 z-10 flex-grow-0 flex-shrink-0 my-36 p-6 bg-white max-w-md hidden sm:block">
             <h3 className="text-3xl mb-4 font-semibold">Refer-a-Friend Program</h3>
             <p className="mb-4">
               Invite friends to enroll in HP Instant Ink too by sharing your
@@ -23,12 +23,24 @@ const Refer = () => {
             </p>
             <Link href="" passHref>
               <button type="submit" className="rounded-xl text-base w-36 h-12 my-auto mr-2 bg-blue-700 opacity-80 hover:opacity-100">
-              <p className="text-white">Learn More</p>
-          </button>
-        </Link>
+                <p className="text-white">Learn More</p>
+              </button>
+            </Link>
           </div>
-
         </div>
+        <div className="absolute transform -translate-y-100 my-36 p-6 bg-white max-w-md block sm:hidden">
+            <h3 className="text-3xl mb-4 font-semibold">Refer-a-Friend Program</h3>
+            <p className="mb-4">
+              Invite friends to enroll in HP Instant Ink too by sharing your
+              unique code. For every friend who signs up, we will give you both
+              FREE MONTHS of service.
+            </p>
+            <Link href="" passHref>
+              <button type="submit" className="rounded-xl text-base w-36 h-12 my-auto mr-2 bg-blue-700 opacity-80 hover:opacity-100">
+                <p className="text-white">Learn More</p>
+              </button>
+            </Link>
+          </div>
     </div>
   );
 }
